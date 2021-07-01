5d5d82267f907173858fad11feffd6ca4c3642bb
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2021-07-01 19:48:52 +0300

    Изменил метод передачи Telegam API токена

--- PickIDbot.py
+++ PickIDbot.py
@@ -2,10 +2,10 @@
 	PickIdBot - tool for receiving the sent type ID
 	yepiwt, 2021
 """
-
+import os
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = ""
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
